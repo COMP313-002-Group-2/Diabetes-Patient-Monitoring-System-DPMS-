6ecfac0ff2fe1cf63279c3dc7a1bf71279dadae8
sofiamehta03 <sofiamehtaaa@gmail.com>
2023-11-11 11:10:58 -0500

    about us page

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,25 +1,34 @@
 import React from 'react';
+import myImage from '../assets/aboutus.jpeg';
 
 const AboutUs = () => {
   return (
     <div>
-      <h1>About Us</h1>
+      <h1 style={{ textAlign: 'center' }}>About Us</h1>
+      <h2 style={{ textAlign: 'center' }}>Welcome to our website!</h2>
 
-     < p>
-        Welcome to our website! We are a passionate team dedicated to providing high-quality products and services to our customers. Our mission is to make a positive impact on the world by delivering innovative solutions and exceptional experiences.
-      </p>
-      <p>
-        We believe in transparency, integrity, and customer satisfaction. Our team is committed to exceeding your expectations and building long-lasting relationships with our valued clients.
-      </p>
-      <p>In today's digital age, many healthcare sectors have leveraged technology to provide efficient, streamlined, and accessible services to patients. However, one glaring issue remains unaddressed: the comprehensive management of diabetes. This technical problem spurred the creation of the Diabetes Patient Management System (DPMS).
-    The DPMS project was undertaken primarily due to the observed gaps in diabetic care. While there are myriad tools for glucose monitoring or booking appointments, a unified platform that consolidates all aspects of diabetic management, including emergency services and physician-patient interaction, was notably absent. This report dives deep into the components of DPMS, detailing its functionalities and highlighting its unique features. For clarity, this document focuses mainly on the digital platform's development and user interaction, omitting intricate technicalities that might overwhelm the lay reader.
-    Our primary objective is to present the DPMS as a holistic solution, aiming to improve patient outcomes and simplify healthcare workflows for diabetic care. As with all groundbreaking ventures, our team encountered several challenges, especially in tailoring the platform to cater to diverse user roles, like patients and ambulance dispatchers, without compromising on simplicity and efficiency. These hurdles have driven us to adopt distinctive approaches in our study, ensuring that DPMS isn't just another application but a revolutionary stride in diabetic management.
-    The Diabetes Patient Monitoring System (DPMS) project aims to revolutionize    diabetes care by providing an integrated online platform for patients and healthcare providers. This system offers secure storage of lab results, medication management, educational resources, insulin dosage guidance, critical results monitoring, and virtual health consultations. While not replacing traditional medical examinations, DPMS seeks to enhance diabetes management, improve patient outcomes, and reduce complications through user-friendly technology and data-driven solutions. Success will be measured by user adoption rates, medication compliance, user engagement, feedback from medical professionals, response times in critical situations, user satisfaction, and improved diabetes management. DPMS is committed to empowering patients and supporting healthcare providers in delivering better diabetes care.
-</p>
-      
-      <p>
-        If you have any questions, feedback, or inquiries, please don't hesitate to contact us. We're here to serve you and look forward to hearing from you.
-      </p>
+      <div>
+        < p>
+          We are a passionate team dedicated to providing high-quality products and services to our customers. Our mission is to make a positive impact on the world by delivering innovative solutions and exceptional experiences. We believe in transparency, integrity, and customer satisfaction.
+        </p>
+      </div>
+
+
+      <div style={{ textAlign: 'center', padding: '20px' }}>
+        <img src={myImage} alt="About Us Page" style={{ display: 'block', marginLeft: 'auto', marginRight: 'auto', width: '300px', height: '200px' }} />
+      </div>
+
+
+      <div>
+        <p>
+
+          Our primary objective is to present the DPMS as a holistic solution, aiming to improve patient outcomes and simplify healthcare workflows for diabetic care.
+          The Diabetes Patient Monitoring System (DPMS) project aims to revolutionize diabetes care by providing an integrated online platform for patients and healthcare providers.
+          This system offers secure storage of lab results, medication management, educational resources, insulin dosage guidance, critical results monitoring, and virtual health consultations.
+        </p>
+      </div>
+      <h3 style={{ textAlign: 'center' }}>If you have any questions, feedback, or inquiries, please don't hesitate to contact us. We're here to serve you and look forward to hearing from you.
+      </h3>
     </div>
   );
 };
