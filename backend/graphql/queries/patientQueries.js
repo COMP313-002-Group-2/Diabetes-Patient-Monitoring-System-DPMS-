c5dc138bd78d827baf011f9e0f30c19a4044c045
vimal-1963 <vmathew7@my.centennialcollege.ca>
2023-11-28 11:09:38 -0500

    edit patient details completed

--- backend/graphql/queries/patientQueries.js
+++ backend/graphql/queries/patientQueries.js
@@ -1,5 +1,6 @@
 import { GraphQLList, GraphQLString } from 'graphql';
 import Patient from '../../models/patient.js';
+import User from '../../models/user.js'
 import { PatientType } from '../types/patientType.js';
 
 const PatientQueries = {
@@ -7,7 +8,7 @@ const PatientQueries = {
         type:PatientType,
         args:{id: { type: GraphQLString } },
         async  resolve (parent, args) {
-            const existingPatient = await Patient.findOne({userId:args.id})
+            const existingPatient = await User.findOne({_id:args.id})
             console.log(existingPatient)
             const {userId,address1,address2,city,phoneNumber,postalCode} = existingPatient;
             return {
