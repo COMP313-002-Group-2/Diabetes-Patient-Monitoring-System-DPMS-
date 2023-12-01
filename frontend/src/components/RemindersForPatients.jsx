32d63acbadd13cdf2f68f4f3928b4969b7078f36
Thedyson <90479286+ThedysonLuzon@users.noreply.github.com>
2023-12-01 01:26:49 -0500

    added alertremindersscreen and reminders viewed by patient

---
+++ frontend/src/components/RemindersForPatients.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useQuery } from '@apollo/client';
+import { getRemindersByPatient } from '../graphql/queries';
+
+const RemindersForPatient = ({ patientId }) => {
+  // Fetch reminders for the patient using patientId
+  const { loading, error, data } = useQuery(getRemindersByPatient, {
+    variables: { patientId: patientId },
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return (
+    <div className="row">
+      {data && data.getRemindersByPatient.map((reminder) => (
+        <div className="col-md-4 mb-3" key={reminder._id}> {/* Adjust column size as needed */}
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">{reminder.reminderName}</h5>
+              <p className="card-text">{reminder.reminderDescription}</p>
+              <p className="card-text"><small className="text-muted">Status: {reminder.status}</small></p>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default RemindersForPatient;
