19ddcc1a60a3f2eef67b728ec41ce387d2c696aa
tannabr23596 <tabrah13@my.centennialcollege.ca>
2023-12-13 15:54:32 -0500

    modifying changes

--- backend/graphql/index.js
+++ backend/graphql/index.js
@@ -4,8 +4,6 @@ import userQueries from './queries/userQueries.js';
 import userMutations from './mutations/userMutations.js';
 import reminderQueries from './queries/reminderQueries.js';
 import reminderMutations from './mutations/reminderMutations.js';
-import bloodchemQueries from './queries/bloodchemQueries.js';
-import bloodchemMutations from './mutations/bloodchemMutations.js';
 import ambulancemutation  from './mutations/ambulanceSchemas.js';
 import ambulanceRequestMutation from './mutations/ambulanceRequestSchemas.js';
 import ambulanceRequestQueries from './queries/ambulanceRequestQueries.js';
@@ -24,7 +22,6 @@ const RootQuery = new GraphQLObjectType({
     ...ArticleQueries,
     ...userQueries,
     ...reminderQueries,
-    ...bloodchemQueries,
     ...ambulanceRequestQueries,
     ...emergencyRequestQueries,
     ...appointmentQueries,
@@ -40,7 +37,6 @@ const Mutation = new GraphQLObjectType({
     ...reminderMutations,
     ...ambulancemutation,
     ...ambulanceRequestMutation,
-    ...bloodchemMutations,
     ...emergencyRequestMutations,
     ...appointmentMutations,
     ...labMutations,
